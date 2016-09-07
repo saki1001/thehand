c1737b284eae4328c83b6094600279cc0076b231
Saki Sato <saki@rayogram.com>
2016-09-07 11:32:15 -0400

    adding wp 4.6, incl custom theme, ACF and Show posts plugins

---
+++ wp-content/themes/thehandspace/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package The_Hand_Space
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'thehandspace' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'thehandspace' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'thehandspace' ), 'thehandspace', '<a href="http://sakisato.com" rel="designer">Saki Sato</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
