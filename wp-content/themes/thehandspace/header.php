386b684a8bbd3089cb33798f98057e0f22206bc3
Saki Sato <saki@rayogram.com>
2016-09-18 23:20:24 -0400

    fixing some footer mobiel styles and added svg logo

--- wp-content/themes/thehandspace/header.php
+++ wp-content/themes/thehandspace/header.php
@@ -53,6 +53,9 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'thehandspace' ); ?></a>
 
 	<header id="header" class="site-header" role="banner">
+
+		<img id="logo" src="<?php echo get_template_directory_uri(); ?>/images/hand-logo.svg" width="50" height="50" alt="The Hand" />
+
 		<div id="branding" class="site-branding">
 			<?php
 			if ( is_front_page() || is_home() ) : ?>
